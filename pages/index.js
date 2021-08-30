8cbbb7032f5ae3350527084ca9c2d92112462412
c0nect <lucas.cavalcant.06@gmail.com>
2021-08-30 00:16:38 -0300

    finished...

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,64 @@
 import Head from 'next/head'
 
-export default function Home() {
+const Logo = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <img src="/logo.png" alt="logotipo" width={150} className="p-2 hidden md:inline-block" />
+      <img src="/codar.png" alt="logotipo" width={50} className="p-2 md:hidden" />
+    </>
+  )
+}
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+const Menu = () => {
+  return (
+    <div className="p-2 flex items-center">
+      <a href="http://github.com/c0nect" target="_blank">
+        github
+      </a>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <a href="http://instagram.com/lucasss.cc" target="_blank">
+        instagram
+      </a>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <a href="http://twitter.com/lucasssdotcc" target="_blank">
+        twitter
+      </a>
     </div>
   )
 }
+
+const Banner = () => {
+  return (
+    <div className="container max-w-5xl m-auto p-6 flex-1 flex flex-col justify-center items-center sm:items-start text-center sm:text-left">
+      <span className="text-gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+      <div className="text-4xl font-extrabold leading-snug">
+        Developer <span className="text-green-500">software</span> from scrath
+      </div>
+      <span className="text-gray">Maiores, totam, officia nostrum pariatur tempore possimus ad cumque quisquam accusamus quas inventore</span>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className="p-2 container max-w-5xl m-auto flex justify-between item-center" >
+      <Logo />
+      <Menu />
+    </div>
+  )
+}
+
+const Home = () => {
+  return (
+    <div className="flex flex-col flex-1" style={{ background: 'url(/bg.png) no-repeat center/cover' }} >
+      <Head>
+        <title>landing page | tailwind.css</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <Banner />
+    </div>
+  )
+}
+
+export default Home
