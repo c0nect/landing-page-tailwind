d21eb30b0a7ffa44685a5e659f5bbb6e956d3277
Lucas Cavalcante <39775168+c0nect@users.noreply.github.com>
2021-08-30 00:38:04 -0300

    add newsletter footer

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,27 @@ const Header = () => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div className="w-full bg-black py-5 relative overflow-hidden">
+      <div class="container mx-auto z-10 relative">
+          <h1 class="xl:text-1xl lg:text-1xl text-center xl:text-left text-white font-bold mb-8">Sign up for product news and updates</h1>
+          <div class="flex items-center mx-auto xl:mx-0 justify-between xl:w-1/3 lg:w-1/3 md:w-1/3 lg:mx-auto w-10/12 border-b border-gray-400 z-10">
+              <input type="email" placeholder="Enter your best e-mail" class="bg-transparent focus:outline-none text-white w-11/12 pb-4" id="email" aria-label="email" />
+              <button aria-label="submit" role="button" class="mb-3 cursor-pointer focus:outline-none focus:ring-2 focus:ring-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CBD5E0" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <line x1="5" y1="12" x2="19" y2="12" />
+                      <line x1="15" y1="16" x2="19" y2="12" />
+                      <line x1="15" y1="8" x2="19" y2="12" />
+                  </svg>
+              </button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
 const Home = () => {
   return (
     <div className="flex flex-col flex-1" style={{ background: 'url(/bg.png) no-repeat center/cover' }} >
@@ -57,6 +78,7 @@ const Home = () => {
       </Head>
       <Header/>
       <Banner />
+      <Footer></Footer>
     </div>
   )
 }
